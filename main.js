0938a0329a8f641934eee5b5c3adc57db5c1ae8b
Nritya Kamath <nritya.k@gmail.com>
2019-05-27 15:51:46 -0700

    Assignment 5 Complete

---
+++ main.js
@@ -0,0 +1,19 @@
+const greetings = require("./greeting.js");
+const farewells = require("./farewell.js");
+
+
+// "Hello, Goodbye"
+greetings.greetingEnglish();
+farewells.farewellEnglish();
+
+// "Konichiwa, Sayōnara"
+greetings.greetingJapanese();
+farewells.farewellJapanese();
+
+// "Hola, Adiós"
+greetings.greetingSpanish();
+farewells.farewellSpanish();
+
+// "Halló, Bless"
+greetings.greetingIcelandic();
+farewells.farewellIcelandic();
